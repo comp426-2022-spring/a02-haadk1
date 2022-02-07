1e1e33ca3c710d67cc2ba9a850b8928f138173aa
Haad Khan <haadk@live.unc.edu>
2022-02-07 15:54:14 -0500

    made a fake coin slip

--- modules/coin.mjs
+++ modules/coin.mjs
@@ -10,13 +10,17 @@
  * @returns {string} 
  * 
  * example: coinFlip()
- * returns: heads
+ * returns: heads | tails
  * 
  */
 
-function coinFlip() {
-
+export function coinFlip() {
+return (Math.floor(Math.random() * 2) == 0) ? 'heads' : 'tails';
 }
+
+
+//export{ coinFlip }
+//console.log(coinFlip())
 
 /** Multiple coin flips
  * 
