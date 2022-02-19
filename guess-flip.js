a9e34e73c84292c98236719971b017090ac9722c
Haad Khan <haadk@live.unc.edu>
2022-02-19 00:11:20 -0500

    yes

--- guess-flip.js
+++ guess-flip.js
@@ -1,5 +1,5 @@
 import minimist from 'minimist'
-import{coinFlips, coinFlip, countFlips,flipACoin} from './modules/coin.mjs'
+import { flipACoin } from './modules/coin.mjs';
 
 const args= minimist(process.argv.length.slice(2))
 const call = args["call"] || 1
