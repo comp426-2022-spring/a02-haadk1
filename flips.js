6a42f7b03af3941d2fe64bd3153c83585e5a52c6
Haad Khan <haadk@live.unc.edu>
2022-02-28 15:38:27 -0500

    yes

--- flips.js
+++ flips.js
@@ -5,7 +5,7 @@ import minimist from 'minimist';
 const args= minimist(process.argv.slice(2))
 const num = args["number"] || 1
 
-const result = coinFlips(num)
+var result = coinFlips(num)
 
 console.log(result)
 console.log(countFlips(result))
