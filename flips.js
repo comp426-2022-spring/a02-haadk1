fc1b7b58dfe53911eec2ce78c5fd9a1f55c34d56
Haad Khan <haadk@live.unc.edu>
2022-03-22 15:59:51 -0400

    added if null

--- flips.js
+++ flips.js
@@ -5,7 +5,15 @@ import minimist from 'minimist';
 const args= minimist(process.argv.slice(2))
 const num = args["number"] || 1
 
-const result = coinFlips(num)
+var result = []
 
-console.log(result)
-console.log(countFlips(result))
+if(num != null ) {
+    result = coinFlips(num)
+    console.log(result)
+    console.log(countFlips(result))
+}
+else {
+    result = coinFlips(1)
+    console.log(result)
+    console.log(countFlips(result))
+}
